d6c1e6b29027ad6b98e14818f4839e3295a7ab67
MatsuSakura <lujiahao0994@gmail.com>
2022-06-15 17:00:53 +0800

    V2.4 add modifyQus

--- src/main/java/ljh/zijin/controller/QuestionController.java
+++ src/main/java/ljh/zijin/controller/QuestionController.java
@@ -27,22 +27,6 @@ public class QuestionController {
         Pager page=questionService.getQuestionByPage(pageNo,pageSize);
         return StringUtil.getJson(page);
     }
-
-    @RequestMapping(value = "/getQuestions.action",produces = "text/html;charset=utf-8")
-    @ResponseBody
-    public String getQuestion(){
-        List<Integer> ids=questionService.getQuestionIds();
-        ObjectMapper mapper = new ObjectMapper();
-        return StringUtil.getJson(ids);
-    }
-
-    @RequestMapping(value = "/getQuestionById.action",produces = "text/html;charset=utf-8")
-    @ResponseBody
-    public String getQuestionById(Integer id){
-        Question question=questionService.getQuestionById(id);
-        ObjectMapper mapper = new ObjectMapper();
-        return StringUtil.getJson(question);
-    }
     @RequestMapping(value = "/addOrModify.action",produces = "text/html;charset=utf-8" )
     public String addOrModify(Question question, Model model, Integer pageNo){
         questionService.addOrModify(question);
@@ -50,7 +34,26 @@ public class QuestionController {
         return "examManager";
     }
     @RequestMapping(value = "/removeQuestionById.action")
+    @ResponseBody
     public String removeQuestionById(Integer id){
-        return "removeQuestion";
+        questionService.removeQuestionById(id);
+        return "ok";
     }
+    @RequestMapping(value = "/getQuestions.action", produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String getQuestion(){
+        //获取此次考试的20道题目
+        List<Integer> ids = questionService.getQuestionIds();
+        //将list转为json格式的字符串返回到页面上
+        //使用jackson.jar中提供的映射类实现
+        ObjectMapper mapper = new ObjectMapper();
+        return StringUtil.getJson(ids); //不是简单的字符串了，是json格式的对象
+    }
+    @RequestMapping(value = "/getQuestionById.action", produces = "text/html;charset=utf-8")
+    @ResponseBody
+    public String getQuestionById(Integer id){
+        Question question = questionService.getQuestionById(id);
+        return StringUtil.getJson(question);
+    }
+
 }
